e4ee7be490211828b2a3ea06cc5322f11319d674
Chuan Cheng <c_chuan23@hotmail.com>
2019-08-20 16:31:06 +0100

    Add test for orderLanding and orderTakeOff

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,28 +1,53 @@
-describe("Airport", function() {
-    
-    var airport = new Airport();
-    
-        beforeEach(function() {
-    
-            landedPlane = {
-                takeOff: function() {},
-                isFlying: function() {
-                    return false;
-                }
-            };
-        });
-    
-    
+describe("Airport class", function() {
+
+
+
         describe("#orderTakeOff", function() {
-    
+
+          let airport = new Airport();
+
+          var landedPlane = {
+              takeOff: function() {},
+              isFlying: function() {
+                  return false;
+              }
+          };
+
             it("makes a plane take off", function() {
-    
+
                 spyOn(landedPlane, 'takeOff');
                 airport.orderTakeOff(landedPlane);
                 expect(landedPlane.takeOff).toHaveBeenCalled();
-    
+
             });
-            
+
         });
-    
+
+        describe("#orderLanding", function() {
+
+            let airport = new Airport();
+
+            var flyingPlane = {
+
+              takeOff: function() {},
+              isFlying: function() {
+                return true;
+              },
+              land: function(){}
+
+            };
+
+            it("makes a plane land", function() {
+
+                spyOn(flyingPlane, 'land');
+                airport.orderLanding(flyingPlane);
+                expect(flyingPlane.land).toHaveBeenCalled();
+
+            });
+
+        });
+
+
+
+
     });
--- src/airport.js
+++ src/airport.js
@@ -1,1 +1,16 @@
 function Airport() {};
+//
+//   this.orderLanding:  function(plane){
+//     plane.land();
+//   },
+//
+//   this.orderTakeoff:  function(plane){
+//     plane.takeoff();
+//   }
+//
+//
+// };
+
+
+Airport.prototype.orderLanding = function(plane){plane.land();};
+Airport.prototype.orderTakeOff = function(plane){plane.takeOff();};
